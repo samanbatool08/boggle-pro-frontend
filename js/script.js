a88c65227f97439097a578898d7f3177a58c559c
Your Name <samanbatool08@gmail.com>
2020-02-07 15:28:55 -0500

    no changes

--- js/script.js
+++ js/script.js
@@ -2,28 +2,23 @@ console.log('javascript is running')
 
 const grid = document.querySelector('.container')
 const letterBar = document.getElementById('letter')
-
-let done = false
 let numbers = []
+
 grid.addEventListener('click', function(e) {
     e.target.style.backgroundColor = "red"
     let letterXId = e.target.dataset.xId
     let letterYId = e.target.dataset.yId
-    if (!done) {
-        let letter = e.target.innerText
+    let letter = e.target.innerText
+    if (numbers.includes(letterXId + letterYId)) {
+        alert('You\'ve already played this letter')
+    } else {
+        numbers.push(letterXId + letterYId)
         letterBar.innerText += letter
         numbers.push([letterXId, letterYId])
         done = true
     } else {
         done = false
     }
-    // if (numbers.includes([letterXId, letterYId])) {
-    //     alert('you clicked this')
-    // }
-    // debugger
-    // if (e.target.dataset.xId + 2 === ) {
-
-    // }
 })
 
 // grid.addEventListener('click', function(e) {
