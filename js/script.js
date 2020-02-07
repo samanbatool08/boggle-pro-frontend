2a89d14000351e11c856a0e70e23b9be7cc30fb3
Your Name <samanbatool08@gmail.com>
2020-02-07 15:05:42 -0500

    working on clicks

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 console.log('javascript is running')
+
+const grid = document.querySelector('.container')
+const letterBar = document.getElementById('letter')
+
+let done = false
+let numbers = []
+grid.addEventListener('click', function(e) {
+    e.target.style.backgroundColor = "red"
+    let letterXId = e.target.dataset.xId
+    let letterYId = e.target.dataset.yId
+    if (!done) {
+        let letter = e.target.innerText
+        letterBar.innerText += letter
+        numbers.push([letterXId, letterYId])
+        done = true
+    }
+    // if (numbers.includes([letterXId, letterYId])) {
+    //     alert('you clicked this')
+    // }
+    // debugger
+    // if (e.target.dataset.xId + 2 === ) {
+
+    // }
+})
+
+
+
+// grid.addEventListener('click', function(e) {
+//     if (e.target.dataset.xId + 2 && e.target.dataset.yId + 2)
+// })
